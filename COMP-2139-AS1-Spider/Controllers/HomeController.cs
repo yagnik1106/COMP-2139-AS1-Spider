5441c49907d31f72a23c962bb6cf55267118f597
yagnik1106 <patelyagnik1106@gmail.com>
2022-02-19 16:52:43 -0500

    Hello

---
+++ COMP-2139-AS1-Spider/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using COMP_2139_AS1_Spider.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace COMP_2139_AS1_Spider.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
